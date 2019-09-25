ea8cb675fa3921f753161451bbcfca7da3c3e985
Eishun Kondoh <shun3382@gmail.com>
2019-09-25 12:01:53 +0900

    document

--- lib/gen_batch_server_ex.ex
+++ lib/gen_batch_server_ex.ex
@@ -21,48 +21,127 @@ defmodule GenBatchServer do
   to use `GenBatchServer` to proxy a bunch of processes that want to update
   some resource (such as a `dets` table) that doesn't handle casts.
 
-  Let's start with a code example and then explore the available callbacks.
-  Imagine we want a GenBatchServer that works like a stack, allowing us to push and pop elements:
+  ## How to supervise
 
-        defmodule Stack do
-          use GenBatchServer
+  A `GenBatchServer` can be started under a supervision tree as well as `GenServer`.
+  When we invoke `use GenBatchServer`, it automatically defines a  child_spec/1`
+  function that allows us to start the `Stack` directly under a supervisor.
+  To start a default stack of `[:hello]` under a supervisor, one may do:
 
-          def init(stack) do
-            {:ok, stack}
-          end
+      children = [
+        {Stack, [:hello]}
+      ]
 
-          def handle_batch(batch, state) do
-            do_handle_batch([], batch, state)
-          end
+      Supervisor.start_link(children, strategy: :one_for_all)
 
-          # private functions
+  Note you can also start it simply as `Stack`, which is the same as `{Stack, []}`:
 
-          defp do_handle_batch(actions, [], state),
-            do: {:ok, actions, state}
+      children = [
+        Stack # The same as {Stack, []}
+      ]
 
-          defp do_handle_batch(actions, [{:cast, {:push, element}} | rest], state),
-            do: do_handle_batch(actions, rest, [element | state])
+      Supervisor.start_link(children, strategy: :one_for_all)
 
-          defp do_handle_batch(actions, [{:call, from, :pop} | rest], [head | tail]),
-            do: do_handle_batch([{:reply, from, head} | actions], rest, tail)
+  In both cases, `Stack.start_link/1` is always invoked.
+
+  `use GenBatchServer` also accepts a list of options which configures the child
+  specification and therefore how it runs under a supervisor. The generated `child_spec/1`
+  can be customized with the following options:
+
+    * `:id` - the child specification identifier, defaults to the current module
+    * `:restart` - when the child should be restarted, defaults to `:permanent`
+    * `:shutdown` - how to shut down the child, either immediately or by giving it time to shut down
+
+  For example:
+
+      use GenBatchServer, restart: :transient, shutdown: 10_000
+
+  See the "Child specification" section in the `Supervisor` module for more detailed information.
+  The `@doc` annotation immediately preceding `use GenBatchServer` will be attached to the geenrated
+  `child_spec/1` function.
+
+  ## Name registration
+
+  Both `start_link/3` and `start/3` support the `GenBatchServer` to register
+  a name on start vi  `:name` option. Registered anmes are also automatically cleaned up on
+  termination. The supported values are:
+
+    * an atom: the GenBatchServer is registered locally with the given name using `Proesss.register/2`
+    * `{:global, term}` - the GenBatchServer is registered globally with the given term using
+      the functions in the [`:global` module](http://www.erlang.org/doc/man/global.html).
+    * `{:via, module, term}` - the GenBatchServer is registered with the given mechanism and name.
+      The `:via` option expects a module that exports `registered_name/2`, `unregistered_name/1`, `whereis_name/1`
+      and `send/2`. One such example is the [`:global` module](http://www.erlang.org/doc/man/global.html)
+      which uses these functions for keeping the list of names of processes and their associated PIDs that are
+      available globally for a network of Elixir nodes. Elixir also ships with a local, decentralized and scalable
+      registery called `Registry` for locally storing names that are generated dynamically.
+
+  For example, we could start and register our `Stack` server locally as follows:
+
+      # Start the server and register it locally with name MyStack
+      {:ok, _pid} = GenBatchServer.start_link(Stack, [:hello], name: MyStack)
+
+      # Now messages can be sent directly to MyStack
+      GenBatchServer.call(MyStack, :pop)
+
+  Once the server is started, the remaining functions in this module (`call/3`, `cast/2` and friends)
+  will also accept an atom, or any `{:global, ...}` or `{:via, ...}` tuples.
+  In general, the following formats are supported:
+
+    * a PID
+    * an atom if the server is locally registered
+    * `{atom, node}` if the server is locally registered at another node
+    * `{:global, term}` if the server is globally registered
+    * `{:via, module, name}` if the server is registered through an alternative registry
+
+  If there is an interest to register dynamic names locally, do not use atoms, as atoms are never garbage-collected.
+  For such cases, you can set up your own local registry by using the `Registry` module.
+
+  ## Receiving "regular" messages
+
+  As well as GenServer, the goal of a GenBatchServer is to abstract the "receive" loop for developers,
+  automatically handling system messages and treating regular messages as batching.
+  Therefore, you should never call your own "receive" inside the GenBatchServer callbacks
+  as doing so will cause the GenBatchServer to misbehave.
+
+  Besides the synchronous and asynchronous communication provided by `call/3` and `cast/2`,
+  "regular" messages sent by functions such as `Kernel.send/2`, `Process.send/2`, Process.send_after/4`
+  and similar, can be handled `{:info, any}` message inside the `c:handle_batch/2` callback.
+
+  `{:info, any}` can be used in many situations, such as handling monitor `DOWN` messages sent by `Process.monitor/1`.
+  Another use case for `{:info, any}` is to perform periodic work, with the help of `Process.send_after/4`.
+
+
+      defmodule MyApp.Periodically do
+        use GenBatchServer
+
+        def start_link do
+          GenBatchServer.start_link(__MODULE__, %{})
         end
 
-        # Start the server
-        {:ok, pid} = GenBatchServer.start_link(Stack, [:hello])
+        @impl true
+        def init(state) do
+          # Schedule work to be performed on start
+          schedule_work()
+          {:ok, state}
+        end
 
-        # This is the client
-        GenBatchServer.call(pid, :pop)
+        @impl true
+        def handle_batch([{:info, :work}], state) do
+          # Do the desired work here
+          # ...
 
-        GenBatchServer.cast(pid, {:push, :world})
+          # Reschedule once more
+          schedule_work()
 
-        GenBatchServer.call(pid, :pop)
+          {:ok, state}
+        end
 
-  We start our `Stack` by calling `start_link/2`, passing the module
-  with the server implementation and its initial argument (a list
-  representing the stack containing the element `:hello`). We can primarily
-  interact with the server by sending two types of messages. **call**
-  messages expect a reply from the server (and are therefore synchronous)
-  while **cast** messages do not.
+        defp schedule_work do
+          # In 2 hours
+          Process.send_after(self(), :work, 2 * 60 * 60 * 1000)
+        end
+      end
   """
 
   @typedoc "Debug options supported by the `start*` functions"
@@ -71,6 +150,7 @@ defmodule GenBatchServer do
   @typedoc "The GenBatchServer name"
   @type name :: atom | {:global, term} | {:via, module, term}
 
+  @typedoc "Option values used by the `start*` functions"
   @type option ::
           {:debug, debug}
           | {:min_batch_size, non_neg_integer}
@@ -80,10 +160,23 @@ defmodule GenBatchServer do
           | {:spawn_opt, Process.spawn_opt()}
           | {:hibernate_after, timeout}
 
+  @typedoc "Return values of `start*` functions"
   @type on_start :: {:ok, pid} | :ignore | {:error, {:already_started, pid} | term}
 
+  @typedoc """
+  The server reference.
+
+  This is either a plain PID or a value representing a registered name.
+  See the "Name registration" section of this document for more information.
+  """
   @type server :: pid | name | {atom, node}
 
+  @typedoc """
+  Tuple describing the client of a call request.
+
+  `pid` is the PID of the caller and `tag` is a unique term used to identify the
+  call.
+  """
   @type from :: {pid, tag :: term}
 
   @type op ::
@@ -93,6 +186,19 @@ defmodule GenBatchServer do
 
   @type action :: {:reply, from, msg :: any}
 
+  @doc """
+  Invoked when the server is started. `start_link/3` or `start/3` will
+  block until it returns.
+
+  `init_arg` is the argument term (second argument) passed to `start_link/3`.
+
+  Returning `{:ok, state}` will cause `start_link/3` to return
+  `{:ok, pid}` and the process to enter its loop.
+
+  Returning `{:stop, reason}` will cause `start_link/3` to return
+  `{:error, reason}` and the process to exit with reason `reason` without
+  entering the loop or calling `c:terminate/2`.
+  """
   @callback init(init_arg :: term) ::
               {:ok, state}
               | {:stop, reason :: any}
@@ -104,8 +210,75 @@ defmodule GenBatchServer do
               | {:stop, reason :: any}
             when state: any
 
+  @doc """
+  Invoked when the server is about to exit. It should do any cleanup required.
+
+  `reason` is exit reason and `state` is the current state of the `GenBatchServer`.
+  The return value is ignored.
+
+  `c:terminate/2` is called if a callback (except `c:init/1`) does one of the
+  following:
+
+    * returns a `:stop` tuple
+    * raises
+    * calls `Kernel.exit/1`
+    * returns an invalid value
+    * the `GenBatchServer` traps exits (using `Process.flag/2`) *and* the parent
+      process sends an exit signal
+
+  If part of a supervision tree, a `GenBatchServer` will receive an exit
+  signal when the tree is shutting down. The exit signal is based on
+  the shutdown strategy in the child's specification, where this
+  value can be:
+
+    * `:brutal_kill`: the `GenBatchServer` is killed and so `c:terminate/2` is not called.
+
+    * a timeout value, where the supervisor will send the exit signal `:shutdown` and
+      the `GenBatchServer` will have the duration of the timeout to terminate.
+      If after duration of this timeout the process is still alive, it will be killed
+      immediately.
+
+  For a more in-depth explanation, please read the "Shutdown values (:shutdown)"
+  section in the `Supervisor` module.
+
+  If the `GenBatchServer` receives an exit signal (that is not `:normal`) from any
+  process when it is not trapping exits it will exit abruptly with the same
+  reason and so not call `c:terminate/2`. Note that a process does *NOT* trap
+  exits by default and an exit signal is sent when a linked process exits or its
+  node is disconnected.
+
+  Therefore it is not guaranteed that `c:terminate/2` is called when a `GenBatchServer`
+  exits. For such reasons, we usually recommend important clean-up rules to
+  happen in separated processes either by use of monitoring or by links
+  themselves. There is no cleanup needed when the `GenBatchServer` controls a `port` (e.g.
+  `:gen_tcp.socket`) or `t:File.io_device/0`, because these will be closed on
+  receiving a `GenBatchServer`'s exit signal and do not need to be closed manually
+  in `c:terminate/2`.
+
+  If `reason` is neither `:normal`, `:shutdown`, nor `{:shutdown, term}` an error is
+  logged.
+
+  This callback is optional.
+  """
   @callback terminate(reason :: any, state) :: any when state: any
 
+  @doc """
+  Invoked in some cases to retrieve a formatted version of the `GenBatchServer` status.
+
+  This callback can be useful to control the *appearance* of the status of the
+  `GenBatchServer`. For example, it can be used to return a compact representation of
+  the `GenBatchServer`'s state to avoid having large state terms printed.
+
+  * one of `:sys.get_status/1` or `:sys.get_status/2` is invoked to get the
+  status of the `GenBatchServer`; in such cases, `reason` is `:normal`
+
+  * the `GenBatchServer` terminates abnormally and logs an error; in such cases,
+  `reason` is `:terminate`
+
+  `pdict_and_state` is a two-elements list `[pdict, state]` where `pdict` is a
+  list of `{key, value}` tuples representing the current process dictionary of
+  the `GenBatchServer` and `state` is the current state of the `GenBatchServer`.
+  """
   @callback format_status(state) :: any when state: any
 
   @optional_callbacks handle_batch: 2,
@@ -120,6 +293,7 @@ defmodule GenBatchServer do
       if Module.get_attribute(__MODULE__, :doc) == nil do
         @doc """
         Returns a specification to start this module under a supervisor.
+
         See `Supervisor`.
         """
       end
@@ -186,6 +360,55 @@ defmodule GenBatchServer do
     end
   end
 
+  @doc """
+  Starts a `GenBatchServer` process linked to the current process.
+
+  This is often used to start the `GenBatchServer` as part of a supervision tree.
+
+  Once the server is started, the `c:init/1` function of the given `module` is
+  called with `init_arg` as its argument to initialize the server. To ensure a
+  synchronized start-up procedure, this function does not return until `c:init/1`
+  has returned.
+
+  Note that a `GenBatchServer` started with `start_link/3` is linked to the
+  parent process and will exit in case of crashes from the parent. The GenBatchServer
+  will also exit due to the `:normal` reasons in case it is configured to trap
+  exits in the `c:init/1` callback.
+
+  ## Options
+
+    * `:min_batch_size` - the minimum batch size
+
+    * `:max_batch_size` - the maximum batch size
+
+    * `:name` - used for name registration as described in the "Name
+      registration" section in the documentation for `GenBatchServer`
+
+    * `:timeout` - if present, the server is allowed to spend the given number of
+      milliseconds initializing or it will be terminated and the start function
+      will return `{:error, :timeout}`
+
+    * `:debug` - if present, the corresponding function in the [`:sys` module](http://www.erlang.org/doc/man/sys.html) is invoked
+
+    * `:spawn_opt` - if present, its value is passed as options to the
+      underlying process as in `Process.spawn/4`
+
+    * `:hibernate_after` - if present, the GenBatchServer process awaits any message for
+      the given number of milliseconds and if no message is received, the process goes
+      into hibernation automatically (by calling `:proc_lib.hibernate/3`).
+
+  ## Return values
+
+  If the server is successfully created and initialized, this function returns
+  `{:ok, pid}`, where `pid` is the PID of the server. If a process with the
+  specified server name already exists, this function returns
+  `{:error, {:already_started, pid}}` with the PID of that process.
+
+  If the `c:init/1` callback fails with `reason`, this function returns
+  `{:error, reason}`. Otherwise, if it returns `{:stop, reason}`
+  or `:ignore`, the process is terminated and this function returns
+  `{:error, reason}` or `:ignore`, respectively.
+  """
   @spec start_link(module, any, [option]) :: on_start
   def start_link(module, init_arg, options \\ [])
 
@@ -194,6 +417,11 @@ defmodule GenBatchServer do
     do_start(:link, module, init_arg, put_default_options(options))
   end
 
+  @doc """
+  Starts a `GenBatchServer` process without links (outside of a supervision tree).
+
+  See `start_link/3` for more information.
+  """
   @spec start(module, any, [option]) :: on_start
   def start(module, init_arg, options \\ [])
 
@@ -239,6 +467,17 @@ defmodule GenBatchServer do
     end
   end
 
+  @doc """
+  Synchronously stops the server with the given `reason`.
+
+  The `c:terminate/2` callback of the given `server` will be invoked before
+  exiting. This function returns `:ok` if the server terminates with the
+  given reason; if it terminates with another reason, the call exits.
+
+  This function keeps OTP semantics regarding error reporting.
+  If the reason is any other than `:normal`, `:shutdown` or
+  `{:shutdown, _}`, an error report is logged.
+  """
   @spec stop(server, reason :: term, timeout) :: :ok
   def stop(server, reason \\ :normal, timeout \\ :infinity)
 
@@ -260,6 +499,11 @@ defmodule GenBatchServer do
     end
   end
 
+  @doc """
+  Makes a synchronous call to the `gen_batch_server` and waits for the response provided
+  by `Module:handle_batch/2`.
+  The timeout is optional and defaults to 5000ms.
+  """
   @spec call(server, term, timeout) :: term
   def call(server, request, timeout \\ 5000)
       when (is_integer(timeout) and timeout >= 0) or timeout == :infinity do
@@ -282,6 +526,11 @@ defmodule GenBatchServer do
     end
   end
 
+  @doc """
+  Sends an asynchronous request to the `gen_batch_server returning` `ok` immediately.
+  The request tuple (`{cast, Request}`) is included in the list of operations passed
+  to `Module:handle_batch/2`.
+  """
   @spec cast(server, term) :: :ok
   def cast(server, request)
 
@@ -308,6 +557,11 @@ defmodule GenBatchServer do
   defp cast_msg(req),
     do: {:"$gen_cast", req}
 
+  @doc """
+  Sends an asynchronous request to the `gen_batch_server returning` `ok` immediately.
+  The request tuple (`{cast, Request}`) is included in the list of operations passed
+  to `Module:handle_batch/2`.
+  """
   @spec cast_batch(server, term) :: :ok
   def cast_batch({:global, name}, batch) when is_list(batch) do
     :global.send(name, cast_batch_msg(batch))
@@ -339,13 +593,13 @@ defmodule GenBatchServer do
   end
 
   @doc """
-  Returns the `pid` or `{name, node}` of a GenServer process, or `nil` if
+  Returns the `pid` or `{name, node}` of a GenBatchServer process, or `nil` if
   no process is associated with the given `server`.
   ## Examples
   For example, to lookup a server process, monitor it and send a cast to it:
-      process = GenServer.whereis(server)
+      process = GenBatchServer.whereis(server)
       monitor = Process.monitor(process)
-      GenServer.cast(process, :hello)
+      GenBatchServer.cast(process, :hello)
   """
   @spec whereis(server) :: pid | {atom, node} | nil
   def whereis(server)
